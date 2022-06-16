0c35a9d759336c210a2e27943bb814e3e24f01c7
brandon <brandon.l.merry@gmail.com>
2022-06-16 19:46:15 +0700

    .

--- server/services/render.js
+++ server/services/render.js
@@ -21,7 +21,7 @@ exports.add_user = (req, res) =>{
 // this returns all data from DB into the form for updating
 
 exports.update_user = (req, res) =>{
-    axios.get(' https://crud-app-sales.herokuapp.com/', { params : { id : req.query.id }})
+    axios.get('https://git.heroku.com/crud-app-sales.git', { params : { id : req.query.id }})
         .then(function(userdata){
             res.render("update_user", { user : userdata.data})
         })
